bee47b004e87577898ca675727d89f34638527ef
Odraode2 <edoardo1.storti@mail.polimi.it>
2026-07-30 19:05:12 +0200

    automation: file position

--- gpu/kuda_kernels/globalbounding.cu
+++ gpu/kuda_kernels/globalbounding.cu
@@ -1,7 +1,7 @@
 #include "globalbounding.hpp"
 #include <thrust/device_vector.h>
 #include <thrust/transform_reduce.h>
-#include <thrust/zip_iterator.h>
+#include <thrust/iterator/zip_iterator.h>
 #include <thrust/tuple.h>
 #include <thrust/execution_policy.h>
 #include <cmath>
